465ab8fc7510d4c587d11f8c22e15380fee65dea
pfoster <paulfostergameroom@gmail.com>
2019-04-16 15:36:26 -0500

    add conditional then functions and always handler to subscribe function

--- FutureTestsShared/FutureTests.swift
+++ FutureTestsShared/FutureTests.swift
@@ -44,6 +44,38 @@ class FutureTests: XCTestCase {
         wait(for: [expectation], timeout: 5.0)
     }
     
+    func testAlways() {
+        let future = Future<Int>(FutureTestError.error)
+        
+        let expectation = XCTestExpectation(description: "Future")
+        
+        future.subscribe({ (value) in
+            XCTFail()
+        }, always: {
+            expectation.fulfill()
+        }) { (error) in
+            
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testAlwaysTwo() {
+        let future = Future<Int>(1)
+        
+        let expectation = XCTestExpectation(description: "Future")
+        
+        future.subscribe({ (value) in
+            XCTAssertEqual(value, 1)
+        }, always: {
+            expectation.fulfill()
+        }) { (error) in
+            XCTFail()
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
     func testTransform() {
         let future = Future<String>("TEST")
         
@@ -71,6 +103,68 @@ class FutureTests: XCTestCase {
         }).subscribe({ (count) in
             expectation.fulfill()
             XCTAssertEqual(count, 4)
+        }) { (error) in
+            XCTFail(error.localizedDescription)
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testConditionalThen() {
+        let future = Future<String>("TEST")
+        
+        let expectation = XCTestExpectation(description: "Future")
+        
+        future.then(condition: false, ifTrue: { (value) -> Future<(Int?, Bool?)> in
+            Future((value.lengthOfBytes(using: String.Encoding.utf8), nil))
+        }) { (value) -> Future<(Int?, Bool?)> in
+            Future((nil, true))
+        }.subscribe({ (values) in
+            expectation.fulfill()
+            XCTAssert(values.1!)
+            XCTAssertNil(values.0)
+        }) { (error) in
+            XCTFail(error.localizedDescription)
+        }
+        
+        future.then(condition: true, ifTrue: { (value) -> Future<(Int?, Bool?)> in
+            Future((value.lengthOfBytes(using: String.Encoding.utf8), nil))
+        }) { (value) -> Future<(Int?, Bool?)> in
+            Future((nil, true))
+        }.subscribe({ (values) in
+            expectation.fulfill()
+            XCTAssertEqual(values.0, 4)
+            XCTAssertNil(values.1)
+        }) { (error) in
+            XCTFail(error.localizedDescription)
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+    }
+    
+    func testConditionalThenTwo() {
+        let future = Future<String>("TEST")
+        
+        let expectation = XCTestExpectation(description: "Future")
+        
+        future.then(condition: false, ifTrue: { (value) -> Future<Int?> in
+            Future(value.lengthOfBytes(using: String.Encoding.utf8))
+        }) { (value) -> Future<Int?> in
+            Future(nil)
+        }.subscribe({ (value) in
+            expectation.fulfill()
+            XCTAssertNil(value)
+        }) { (error) in
+            XCTFail(error.localizedDescription)
+        }
+        
+        future.then(condition: true, ifTrue: { (value) -> Future<Int?> in
+            Future(value.lengthOfBytes(using: String.Encoding.utf8))
+        }) { (value) -> Future<Int?> in
+            Future(nil)
+        }.subscribe({ (value) in
+            expectation.fulfill()
+            XCTAssertEqual(value, 4)
         }) { (error) in
             XCTFail(error.localizedDescription)
         }
